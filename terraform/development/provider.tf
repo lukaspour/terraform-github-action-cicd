624c51bac37c74fe75645dbd4034c0ad5bfab107
Lukas Pour <lukaspour@gmail.com>
2019-12-18 14:21:27 +0100

    add bastion to dev/stage

---
+++ terraform/development/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region              = "eu-west-1"
+  allowed_account_ids = ["XXXXXX"]
+}
+
+terraform {
+  required_version = "0.12.12"
+
+  backend "s3" {
+    bucket         = "neo-terraform-state"
+    key            = "neo-dev"
+    region         = "eu-west-1"
+    dynamodb_table = "neo-terraform-state"
+    kms_key_id     = "arn:aws:kms:eu-west-1:XXXXXX:key/ee51172c-e7bd-46ba-a540-fc66baac4caa"
+    encrypt        = "true"
+    acl            = "bucket-owner-full-control"
+  }
+}
